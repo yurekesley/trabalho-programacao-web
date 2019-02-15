7915fc3afb57a29615a18f4a986fa174effcda7e
MDIAS\ter01282 <yurekesley@gmail.com>
2019-02-15 18:50:16 -0200

    criando projeto front

--- carros-web-api/carros-web-api/src/test/java/br/com/yurekesley/carroswebapi/CarrosWebApiApplicationTests.java
+++ carros-web-api/carros-web-api/src/test/java/br/com/yurekesley/carroswebapi/CarrosWebApiApplicationTests.java
@@ -3,10 +3,12 @@ package br.com.yurekesley.carroswebapi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest("application.test.properties")
+//@TestPropertySource(locations="classpath:application.test.properties")
 public class CarrosWebApiApplicationTests {
 
 	@Test
