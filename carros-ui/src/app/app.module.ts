00b783a2a8dbad2805a47b73641eed7d817a4193
MDIAS\ter01282 <yurekesley@gmail.com>
2019-02-21 13:16:44 -0300

    angular 7

--- carros-ui/src/app/app.module.ts
+++ carros-ui/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { environment as env } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
